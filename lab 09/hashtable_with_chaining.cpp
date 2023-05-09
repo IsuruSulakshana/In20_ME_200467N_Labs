fc25d4f960af58d7070c3d56fa9de4bbdf9c8bd4
Isuru Sulakshana <pererawhis.20@uom.lk>
2023-05-09 19:19:23 +0530

    updated

--- lab 09/hashtable_with_chaining.cpp
+++ lab 09/hashtable_with_chaining.cpp
@@ -2,24 +2,24 @@
 #include "linkedlist.cpp"
 using namespace std;
 
-struct HashTable{
+struct HashTable {
     int MAX_LENGTH = 4;
     int MAX_CHAIN_LENGTH = 2;
-    LinkedList * password = new LinkedList[MAX_LENGTH];
-    bool isFull(){
+    LinkedList* password = new LinkedList[MAX_LENGTH];
+    bool isFull() {
         bool full = true;
         int count = 0;
-        for (int i = 0; i < MAX_LENGTH; i++){
-            if(password[i].head == NULL){
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            if (password[i].head == NULL) {
                 full = false;
             }
         }
         return full;
     }
-    int hashfunc(string user_name){
+    int hashfunc(string user_name) {
         int sum = 0;
         int hash = 0;
-        for(int i = 0;i< user_name.length(); i++){
+        for (int i = 0;i < user_name.length(); i++) {
             sum += int(user_name[i]);
 
         }
@@ -27,68 +27,75 @@ struct HashTable{
 
         return hash;
     }
-    bool is_slot_empty(int hash){
+    bool is_slot_empty(int hash) {
         bool empty;
-        if(password[hash].head==NULL){
+        if (password[hash].head == NULL) {
             empty = true;
         }
-        else{
-            empty=false;
+        else {
+            empty = false;
         }
         return empty;
     }
-    void insert(string user_name,string user_password){
+    void insert(string user_name, string user_password) {
         int hash;
         bool empty;
         hash = hashfunc(user_name);
         empty = is_slot_empty(hash);
-        if(password[hash].length<MAX_CHAIN_LENGTH){
-            password[hash].insert(user_name.data(),user_password.data());
+        if (password[hash].length < MAX_CHAIN_LENGTH) {
+            password[hash].insert(user_name.data(), user_password.data());
 
         }
-        else{
-            cout<<"Linked List reached MAX CAP!\n";
+        else {
+            cout << "Linked List reached MAX CAP!\n";
         }
     }
-    void print_hashtable(){
-        for(int i=0;i<MAX_LENGTH;i++){
-            cout<<"["<<i<<"]-->";
+    void print_hashtable() {
+        for (int i = 0;i < MAX_LENGTH;i++) {
+            cout << "[" << i << "]-->";
             password[i].print_list();
         }
     }
-    void hash_lookup(string user_name){
+    void hash_lookup(string user_name) {
         int hash;
         bool empty;
         hash = hashfunc(user_name);
         empty = is_slot_empty(hash);
-	//add your code below
-	
-	
-	
-	
-	
-	
+        //add your code below
+        if (empty) {
+            cout << "Empty";
+        }
+
+        else {
+            password[hash].search(user_name);
+        }
+
+
+
+
+
+
     }
 };
 
-int main(){
-    HashTable * hashtbl = new HashTable;
-    cout<< hashtbl->isFull()<<"\n";
-    int command=0;
+int main() {
+    HashTable* hashtbl = new HashTable;
+    cout << hashtbl->isFull() << "\n";
+    int command = 0;
     string user_name;
     string password;
-    while (command!=-1){
+    while (command != -1) {
         /* code */
         cout << "Type command: ";
         cin >> command;
-        switch (command){
+        switch (command) {
         case 1:
             /* insert the new item*/
             cout << "Enter user name: ";
             cin >> user_name;
             cout << "Enter password to be saved: ";
             cin >> password;
-            hashtbl->insert(user_name,password);
+            hashtbl->insert(user_name, password);
             break;
         case 2:
             /* hash lookup password*/
@@ -104,9 +111,9 @@ int main(){
             hashtbl->print_hashtable();
             cout << "Exiting...\n";
             break;
-        
+
         }
-    
+
     }
     return 0;
 }
--- lab 09/linkedlist.cpp
+++ lab 09/linkedlist.cpp
@@ -6,44 +6,44 @@ struct Node {
     /* data */
     string user_name;
     string password;
-    Node *link;
+    Node* link;
 };
 
-struct LinkedList{
+struct LinkedList {
 
-    Node * head = NULL;
-    Node * tail = NULL;
+    Node* head = NULL;
+    Node* tail = NULL;
 
     int length = 0;
 
-    void insert(string user_name,string password){
-        if (head==NULL){
-            Node * temp;
+    void insert(string user_name, string password) {
+        if (head == NULL) {
+            Node* temp;
             temp = new Node;
             temp->user_name = user_name;
             temp->password = password;
 
             temp->link = NULL;
-            head = temp; 
+            head = temp;
             length++;
         }
-        else{
+        else {
 
-            Node * temp;
-            Node * prev;
-            Node * tail;
+            Node* temp;
+            Node* prev;
+            Node* tail;
 
             temp = new Node;
             temp->user_name = user_name;
             temp->password = password;
             temp->link = NULL;
             tail = head;
-            while (true){
-                
-                if (tail->link== NULL){
+            while (true) {
+
+                if (tail->link == NULL) {
                     break;
                 }
-                else{
+                else {
                     tail = tail->link;
                 }
             }
@@ -52,30 +52,30 @@ struct LinkedList{
         }
     }
 
-    void search(string user_name){
-        Node * temp = head;
-        for(int i=0;i<length;i++){
-            if(temp->user_name==user_name){
-                cout<<"Password: "<<temp->password<<"\n";
+    void search(string user_name) {
+        Node* temp = head;
+        for (int i = 0;i < length;i++) {
+            if (temp->user_name == user_name) {
+                cout << "Password: " << temp->password << "\n";
             }
-            else{
-                temp = temp->link; 
+            else {
+                temp = temp->link;
             }
         }
     }
 
-    void print_list(){
-        if (head == NULL){
-            cout<<"[]"<<endl;
+    void print_list() {
+        if (head == NULL) {
+            cout << "[]" << endl;
         }
-        else{
-            Node * temp = head;
-            cout<< "[";
-            while (temp != NULL){
-                cout<<temp->user_name<<", ";
+        else {
+            Node* temp = head;
+            cout << "[";
+            while (temp != NULL) {
+                cout << temp->user_name << ", ";
                 temp = temp->link;
             }
-             cout<< "]"<<endl;
+            cout << "]" << endl;
         }
     }
 
